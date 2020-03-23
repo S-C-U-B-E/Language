eef3fdab429b179a0ba3c9358827e75f2abad65f
S-C-U-B-E <sssanyal10@gmail.com>
2020-03-24 01:58:09 +0530

    1.1 Learning to use List View

--- app/src/main/java/com/example/language/NumbersActivity.java
+++ app/src/main/java/com/example/language/NumbersActivity.java
@@ -2,8 +2,13 @@
 package com.example.language;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,6 +16,23 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        words.add("elevem");
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(listAdapter);
 
     }
 }
